46b54c0c28a5e9d5178653953e4204881312be7e
Gwen Bradbury <charmed_89@msn.com>
2020-10-01 11:07:33 +0000

    Made a good start on the README. Tried to alter the code in maps.js so i only have one info window

--- README.md
+++ README.md
@@ -1,4 +1,144 @@
-https://coolors.co/000000-14213d-fca311-e5e5e5-ffffff - color scheme.
+# Into The Blue.
+
+-markdown!
+
+<small><i><a href='http://ecotrust-canada.github.io/markdown-toc/'>Table of contents generated with markdown-toc</a></i></small>
+
+## Into The Blue.
+
+![Apple Devices Picture](./wireframes/)
+
+This is my second milestone project; Designed to exhibit my capabilities and skills in javascript, as a student 
+of Code Institute. (https://codeinstitute.net/)
+The goal was to showcase my skills to potential employers/recruiters, on a topic that I'm deeply interested in.
+My project allows users to discover new divesites within the UK, inspiring them to dive in new locations and more frequently.
+
+## UX.
+
+The purpose of the site is to provide a simple, easy to use format presenting information about some of the UK's best divesites.
+My website is designed for people interested in diving, specifically diving in the UK, and im hopeful my website 
+will give them the motivation towards getting into the water more often and diving somewhere new.
+
+#### Wireframes-
+
+![Wireframe picture](./wireframes/MS2.jpeg)
+
+#### Colour Palette-
+
+https://coolors.co/000000-14213d-fca311-e5e5e5-ffffff
+
+#### User Stories-
+
+* 
+* 
+* 
+
+## Features.
+
+### Exsisting Features-
+
+* _Google Map_ - 
+
+* _Information Windows_ -
+
+* _Wish List_ - 
+
+* _Don't Forget_ -
+
+* _Nav Bar_ -
+
+## Technologies Used.
+
+1. **HTML, or Hyper Text Markup Language:**  Used to construct the page withn this web site.  For further info on this language;   
+ https://developer.mozilla.org/en-US/docs/Web/HTML
+
+2. **CSS, or Cascading Style Sheets:**  Used to style the various elements on the web page via coloring, fonts, spacing, etc.  For further info, see this link;
+ https://www.w3.org/Style/CSS/Overview.en.html
+
+3. **Javascript:** 
+
+4. **Bootstrap:**  A CSS framework that assists the programmer in creating responsive, mobile first front-end web sites. https://getbootstrap.com/
+
+5. **Gitpod:**  An online IDE also used for creating & saving code that runs in a browser, it does not have to be installed on your PC.
+ https://www.gitpod.io/
+
+6. **Git:**  A version control system for tracking changes in source code during software development. https://git-scm.com/
+
+7. **Github:**  A company that provides hosting for software development version control using Git. It is a subsidiary of Microsoft. https://github.
+
+8. **Chrome DevTools:**  A set of web developer tools built directly into the Google Chrome browser. I used these tools constantly thoughout the development cycle.
+ https://developers.google.com/web/tools/chrome-devtools
+
+9. **W3C Markup Validation Service:**  Used to run all html and css code thru a validation process looking for errors; https://validator.w3.org/
+ https://jigsaw.w3.org/css-validator/validator
+
+ ## Testing.
+
+ 1.  http://ami.responsivedesign.is/  has been used to see how the site performs on different Apple devices and their viewports, all pages, links, icons performed as expected on all devices. I also used it to create the AppleDevicesView.png at the top of this Readme.
+
+ 2.  **Laptop**
+     
+      * Google Chrome browser; all pages, links on those pages and footer icon links perform well on all viewport sizes. Developer tools were also used on browser for the various viewport sizes.
+
+ 3.  **Mobile**
+
+      * Safari browser; all pages, links on those pages and footer icon links perform well.
+
+ 4.  **Ipad Pro**
+
+      * Safari browser; all pages, links on those pages and footer icon links perform as they should.
+
+![Apple Devices picture](./wireframes/)
+
+ 5. **Desktop**
+
+      * Google Chrome browser; all pages, links on those pages and footer icon links perform well on all viewport sizes. Developer tools were also used on browser for the various viewport sizes.
+      * Internet Edge; all pages, links on those pages and footer icon links perform well on all viewport sizes. Developer tools were also used on browser for the various viewport sizes.
+      * Mozilla Firefox; all pages, links on those pages and footer icon links perform well on all viewport sizes. Developer tools were also used on browser for the various viewport sizes.        
+
+ 7.  **Open Issues:** 
+
+      *    
+
+ ## Deployment.
+
+1. Created a Github account at https://github.com
+  My account url;    https://github.com/Gwen-Bradbury
+
+2. I uploaded all files to my Github repository located at this url; https://github.com/Gwen-Bradbury/MS2  which is for this individual project.
+
+3. To publish the project to see it on the web, I then went into the Settings on my respository, scrolled down to the heading, GitHub Pages. Under the Source setting, I used the drop-down menu to select master branch as a publishing source and saved it.  Refreshed the github page, and you are then given a url where your page is published;
+   Your site is published at https://gwen-bradbury.github.io/MS2/
+
+4.  To run this code on your local machine, you would go to my respository at 
+https://github.com/Gwen-Bradbury/MS2  and on the home page on the right hand side just above all the files, you will see a green button that says,
+"Clone or download", this button will give you options to clone with HTTPS, open in desktop or download as a zip file.
+To continue with cloning, you would;
+  * Open Git Bash
+  * Change the current working directory to the location where you want the cloned directory to be made.
+  * Type git clone, and then paste this URL; https://github.com/Gwen-Bradbury/MS2.git      Press Enter. Your local clone will be created.
+
+  For more information about the above process; https://help.github.com/en/github/creating-cloning-and-archiving-repositories/cloning-a-repository
+
+## Credits.
+
+### Content-
+
+1. 
+
+### Media-
+
+1. 
+
+### Acknowledgements-
+
+1. My mentor Adegbenga for his help and many other pieces of advice alongside motivation to get me through.`
+
+2. Code Institute's Slack Channels and the many alunni, mentors, tutors and users who contribute to them; the many pinned announcements, pdf files, etc. were of great help, as was the advice and motivation
+given by the many members. https://app.slack.com/client/T0L30B202/C0L316Z96
+
+3. The amazing Code Institute Tutors.
+
 https://app.lucidchart.com - wireframes
 https://getbootstrap.com/docs/4.4/components/navbar/ - navbar
 https://developers.google.com/maps/documentation/javascript/infowindows - map markers and info windows
--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,76 +1,79 @@
 function initMap() {
-        let map = new google.maps.Map(document.getElementById("map"), 
-        {
-            zoom: 4,
-            center: {
-                lat: 51.5074,
-                lng: -0.1278
-            }
-        });
+  let map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: {
+      lat: 51.5074,
+      lng: -0.1278,
+    },
+  });
 
-        let diveSite = [
-        {
-            coordinates: { lat: 54.1388, lng: -2.7174 },
-            content: '<h4>Capernwray</h4><br>Quarry stocked with fish, including trout, perch and at least two sturgeon, and many underwater attractions!<br>Dive with <a href="https://www.dive-site.co.uk/" target="_blank">Capernwray Diving Center</a>'
-        },
-        {
-            coordinates: { lat: 51.1781, lng: -4.6673 },
-            content: '<h4>Lundy Island</h4><br>English island in the Bristol Channel.It boasts a variety of marine habitats, with rare seaweeds, sponges and corals.<br>Dive with <a href="https://www.lundydiving.co.uk/diving/" target="_blank">Lundy Diving</a>'
-        },
-        {
-            coordinates: { lat: 58.9000, lng: -3.0500 },
-            content: '<h4>Scapa Flow</h4><br>Offers an abundance of large, historic wrecks lying in close proximity and shallow, relatively benign diving conditions.<br>Dive with <a href="http://www.scapaflow.com/" target="_blank">Johns Diving Charters</a>'
-        },
-        {
-            coordinates: { lat: 55.6399, lng: -1.6030 },
-            content: '<h4>Farne Islands</h4><br>The grey seal colony at the Farnes numbers about 5,000. They are curious and will often look in on divers in the water and are impressive to watch underwater.Hundreds of ships have been wrecked on the Farnes over the years, providing plenty for wreck divers to look at.<br>Dive with <a href="https://travel.padi.com/d/united-kingdom/" target="_blank">PADI North England</a>'
-        },
-        {
-            coordinates: { lat: 51.7374, lng: -5.2815 },
-            content: '<h4>Skomer Marine Reserve</h4><br>There are wall drops which descend to over 50m, caves and over 500 wrecks to explore.Keep your eyes peeled for dolphins, seals, colourful sea slugs, catfish, lobsters and rare seahorses.<br>Dive with <a href="https://www.visitpembrokeshire.com/activity-listing/west-wales-dive-company-ltd" target="_blank">West Wales Diving Company</a>'
-        },
-        {
-            coordinates: { lat: 57.8167, lng: -8.5833 },
-            content: '<h4>St Kilda</h4><br>A haven of caves, arches and walls covered in marine life with amazing water visibility. It’s a great place to spot seals and puffins diving in the water, huge lobsters, plus beautiful multi-coloured jewelled anemones.<br>Dive with <a href="https://dive.padi.com/unitedkingdom/scotland/" target="_blank">PADI Scotland</a>'
-        },
-        {
-            coordinates: { lat: 50.0500, lng: -5.0500 },
-            content: '<h4>The Manacles</h4><br>It has amazing drop-offs, pinnacles and reefs covered with Gorgonian sea fans, jewel anemones and dead man’s fingers.Watch out for conger eels hiding inside holes in the wrecks.<br>Dive with <a href="https://porthkerris.com/" target="_blank">Porthkerris Divers</a>'
-        },
-        {
-            coordinates: { lat: 53.1460, lng: -4.3559 },
-            content: '<h4>Rhoscolyn Beacon</h4><br>Offers a fantastic array of marine life and wrecks to explore. Rhoscolyn Beacon is one of the best drift diving spots in the UK.<br>Dive with <a href="https://www.sbsribcharter.co.uk/index.php/diving" target="_blank">SBS Rib Charters</a>'
-        },
-        {
-            coordinates: { lat: 55.892, lng: -2.152 },
-            content: '<h4>St Abbs Marine Reserve</h4><br>Just 50m offshore, you’ll find Cathedral Rock, a beautiful double rock arch covered in anemones and dead man’s fingers.<br>Dive with <a href="http://divestabbs.com/" target="_blank">Dive St Abbs</a>'
-        },
-        {
-            coordinates:{ lat: 50.342, lng: -2.2640 },
-            content: '<h4>M2 Portland</h4><br>Ever wanted to dive alongside a sunken submarine? HMS M2 is one of the most unusual submarine wrecks in the UK. Situated three miles off Lyme Bay in Dorset, it lies at around 36m deep and was an underwater aircraft carrier designed to surface, launch one plane and sink into the depths again.<br>Dive with <a href="https://divebeyond.co.uk/" target="_blank">Dive Beyond</a>'
-        }
-    ];
+  let diveSite = [
+    {
+      coordinates: { lat: 54.1388, lng: -2.7174 },
+      content:
+        '<h4>Capernwray</h4><br>Quarry stocked with fish, including trout, perch and at least two sturgeon, and many underwater attractions!<br>Dive with <a href="https://www.dive-site.co.uk/" target="_blank">Capernwray Diving Center</a>',
+    },
+    {
+      coordinates: { lat: 51.1781, lng: -4.6673 },
+      content:
+        '<h4>Lundy Island</h4><br>English island in the Bristol Channel.It boasts a variety of marine habitats, with rare seaweeds, sponges and corals.<br>Dive with <a href="https://www.lundydiving.co.uk/diving/" target="_blank">Lundy Diving</a>',
+    },
+    {
+      coordinates: { lat: 58.9, lng: -3.05 },
+      content:
+        '<h4>Scapa Flow</h4><br>Offers an abundance of large, historic wrecks lying in close proximity and shallow, relatively benign diving conditions.<br>Dive with <a href="http://www.scapaflow.com/" target="_blank">Johns Diving Charters</a>',
+    },
+    {
+      coordinates: { lat: 55.6399, lng: -1.603 },
+      content:
+        '<h4>Farne Islands</h4><br>The grey seal colony at the Farnes numbers about 5,000. They are curious and will often look in on divers in the water and are impressive to watch underwater.Hundreds of ships have been wrecked on the Farnes over the years, providing plenty for wreck divers to look at.<br>Dive with <a href="https://travel.padi.com/d/united-kingdom/" target="_blank">PADI North England</a>',
+    },
+    {
+      coordinates: { lat: 51.7374, lng: -5.2815 },
+      content:
+        '<h4>Skomer Marine Reserve</h4><br>There are wall drops which descend to over 50m, caves and over 500 wrecks to explore.Keep your eyes peeled for dolphins, seals, colourful sea slugs, catfish, lobsters and rare seahorses.<br>Dive with <a href="https://www.visitpembrokeshire.com/activity-listing/west-wales-dive-company-ltd" target="_blank">West Wales Diving Company</a>',
+    },
+    {
+      coordinates: { lat: 57.8167, lng: -8.5833 },
+      content:
+        '<h4>St Kilda</h4><br>A haven of caves, arches and walls covered in marine life with amazing water visibility. It’s a great place to spot seals and puffins diving in the water, huge lobsters, plus beautiful multi-coloured jewelled anemones.<br>Dive with <a href="https://dive.padi.com/unitedkingdom/scotland/" target="_blank">PADI Scotland</a>',
+    },
+    {
+      coordinates: { lat: 50.05, lng: -5.05 },
+      content:
+        '<h4>The Manacles</h4><br>It has amazing drop-offs, pinnacles and reefs covered with Gorgonian sea fans, jewel anemones and dead man’s fingers.Watch out for conger eels hiding inside holes in the wrecks.<br>Dive with <a href="https://porthkerris.com/" target="_blank">Porthkerris Divers</a>',
+    },
+    {
+      coordinates: { lat: 53.146, lng: -4.3559 },
+      content:
+        '<h4>Rhoscolyn Beacon</h4><br>Offers a fantastic array of marine life and wrecks to explore. Rhoscolyn Beacon is one of the best drift diving spots in the UK.<br>Dive with <a href="https://www.sbsribcharter.co.uk/index.php/diving" target="_blank">SBS Rib Charters</a>',
+    },
+    {
+      coordinates: { lat: 55.892, lng: -2.152 },
+      content:
+        '<h4>St Abbs Marine Reserve</h4><br>Just 50m offshore, you’ll find Cathedral Rock, a beautiful double rock arch covered in anemones and dead man’s fingers.<br>Dive with <a href="http://divestabbs.com/" target="_blank">Dive St Abbs</a>',
+    },
+    {
+      coordinates: { lat: 50.342, lng: -2.264 },
+      content:
+        '<h4>M2 Portland</h4><br>Ever wanted to dive alongside a sunken submarine? HMS M2 is one of the most unusual submarine wrecks in the UK. Situated three miles off Lyme Bay in Dorset, it lies at around 36m deep and was an underwater aircraft carrier designed to surface, launch one plane and sink into the depths again.<br>Dive with <a href="https://divebeyond.co.uk/" target="_blank">Dive Beyond</a>',
+    },
+  ];
 
-    for (let i =0; i<diveSite.length; i++){
-            setDivesite(diveSite[i]);
-        }
+  let infoWindow = new google.maps.InfoWindow();
 
-        function setDivesite(props){ 
-            let marker = new google.maps.Marker({
-                position: props.coordinates,
-                map: map,
-                animation: google.maps.Animation.DROP
-              });
-    
-                  let infoWindow = new google.maps.InfoWindow({
-        content: props.content
-        });
+  let marker = new google.maps.Marker();
 
-        marker.addListener('click', function() {
-        infoWindow.open(map, marker); 
-        map.addListener('click', function() {
-        infoWindow.close(map, marker); 
-        });
-    });
-        }
+   for (let i = 0; i < diveSite.length; i++){
+
+    marker.addListener("click", function() {
+       new google.maps.Marker({
+        position: coordinates,
+        map: map,
+       animation: google.maps.Animation.DROP,
+       })
+      infoWindow.setContent(diveSite[i]),
+     infoWindow.open(map, marker)
+      });
     }
+}
